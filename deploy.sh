760442c250b5867857f0e0ad4be6fef8a2f5f5e3
jimo1001 <jimo1001@gmail.com>
2016-04-12 00:03:26 +0900

    small fixes

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,10 @@ set -ex
 
 cd build
 
+git config --global user.name "Travis CI"
+git config --global user.email "nobody@nobody.org"
+
 git init
-git config user.name "system"
-git config user.email "system@example.com"
 git add .
-git commit -m "Auto-Deploy"
+git commit -m "Deploy to Github Pages"
 git push --force --quiet "https://${GH_TOKEN}@${GH_REPO}" master:gh-pages > /dev/null 2>&1
